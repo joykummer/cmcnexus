f403e9028137bba251f7e0a717908b258b73940d
Jannic Veith <jannic.veith@v-online.me>
2020-04-06 17:08:29 +0200

    Add orca urls

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -3,8 +3,7 @@ let baseUrl = '';
 if (process.env.NODE_ENV === 'development') {
 	baseUrl = 'http://0.0.0.0:8000/backend/api/'
 } else {
-	baseUrl = ''
+	baseUrl = 'https://cmc-nexus.propulsion-learn.ch/backend/api/'
 }
 
-baseUrl = ''
 export default baseUrl;
