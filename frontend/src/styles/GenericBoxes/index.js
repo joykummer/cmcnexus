b71c8e5facec5332606c69cc87692c0fcb8763a7
Jannic Veith <jannic.veith@v-online.me>
2020-04-08 10:30:30 +0200

    Fix script to orca, add login

---
+++ frontend/src/styles/GenericBoxes/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const CardBox = styled.div`
+background: #FFFFFF;
+opacity: 0.7;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 5px;
+	`;
