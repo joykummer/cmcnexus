1c8351ecb0bd63d803df8b5f388a616d55cd6e74
Joy Yoo <joyannayoo@gmail.com>
2020-04-08 16:44:38 +0000

    Update views.py

--- backend/apps/organisation/views.py
+++ backend/apps/organisation/views.py
@@ -7,6 +7,10 @@ from .serializer import OrganisationSerializer
 class GetAllOrganisations(ListCreateAPIView):
     queryset = Organisation.objects.all()
     serializer_class = OrganisationSerializer
+    
+        def get_queryset(self):
+        return Organisation.objects.filter(name__icontains=self.request.query_params.get('search', ''))
+
 
 
 class GetUpdateByIdView(RetrieveUpdateAPIView):
