f6f45d9e953d8c6c674587f718d8ace128a4a7c3
Myri <myrimail84@gmail.comm>
2020-04-10 07:58:00 +0200

    paths cleaned

--- backend/orca/urls.py
+++ backend/orca/urls.py
@@ -21,15 +21,14 @@ from rest_framework_simplejwt import views as jwt_views
 api_patterns = [
     path('cases/', include('apps.cases.urls')),
     path('organisations/', include('apps.organisation.urls')),
+    path('organisations/categories/', include('apps.organisation_category.urls')),
     path('users/', include('apps.users.urls_me')),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_refresh')
+    path('token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns = [
     path('backend/admin/', admin.site.urls),
-    path('backend/api/organisations/', include('apps.organisation.urls')),
-    path('backend/api/categories/', include('apps.organisation_category.urls')),
-    path('backend/api/', include(api_patterns))
+    path('backend/api/', include(api_patterns)),
 ]
