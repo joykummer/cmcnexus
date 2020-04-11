c4a166d4a089a79ccb4ce72168ab2ca8fe56748b
Joy Yoo <joyannayoo@gmail.com>
2020-04-11 11:59:40 +0200

    clean and refactor table

---
+++ frontend/src/components/ListOrganisations/listOrganisationsTable.js
@@ -0,0 +1,63 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import {organisationsFunction} from "../../store/actions/organisationsAction";
+import {
+    Table,
+    TableBody,
+    TableData,
+    TableHeader,
+    TableHeaderRow,
+    TableHeaderWrapper,
+    TableRow
+} from "../../styles/Tables";
+
+
+function ListOrganisationsTable(props) {
+
+  useEffect(() => {
+    props.dispatch(organisationsFunction());
+  }, []);
+
+  const headers = [
+      'Name',
+      'Description',
+      'Category',
+      'Tag(s)'
+  ];
+
+  return (
+        <Table>
+          <TableHeaderWrapper>
+            <TableHeaderRow>
+              {headers.map((header) => {
+                return (
+                    <TableHeader>{header}</TableHeader>
+                )
+              })}
+            </TableHeaderRow>
+          </TableHeaderWrapper>
+          <TableBody>
+        {props.organisations
+          ? props.organisations.map(organisation => {
+              return (
+                <TableRow key={organisation.id}>
+                  <TableData>{organisation.name}</TableData>
+                  <TableData>{organisation.description}</TableData>
+                  <TableData>{organisation.category.name}</TableData>
+                  <TableData>{organisation.tag}</TableData>
+                </TableRow>
+              );
+            })
+          : null}
+          </TableBody>
+        </Table>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    organisations: state.organisations
+  };
+};
+
+export default connect(mapStateToProps)(ListOrganisationsTable);
--- frontend/src/styles/Tables/index.js
+++ frontend/src/styles/Tables/index.js
@@ -1,23 +1,31 @@
 import styled from "styled-components";
 
 export const Table = styled.table`
-    width: 75%;
-    height: 75%;
+    width: 60%;
+    height: 70%;
+    border: 1px solid #EBEBEB;
   background: #ffffff;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 5px;
 `;
 
-export const TableHeader = styled.thead`
-    width: 100%;
-    height: 30%;
-    background-color: green;
-    border: 1px solid #A7A7A7;
+export const TableHeaderWrapper = styled.thead`
+`;
+
+export const TableHeader = styled.th`
+    height: 40px;
+`;
+
+export const TableHeaderRow = styled.tr`
+`;
+
+export const TableBody = styled.tbody`
 `;
 
 export const TableRow = styled.tr`
-    width: 100%;
-    height: 10%;
-    background-color: pink;
-    border: 1px solid #A7A7A7;
+    :nth-child(odd) {
+        background: #EBEBEB;
+`;
+
+export const TableData = styled.td`
 `;
