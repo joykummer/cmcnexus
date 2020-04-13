ac32d9fc8d73c8a9a680c943d8c3ee8f8f7028a0
Jannic Veith <jannic.veith@v-online.me>
2020-04-13 18:13:22 +0200

    Fixes

--- frontend/src/components/PrivateRoute/index.js
+++ frontend/src/components/PrivateRoute/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Route, Redirect } from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import {Route, Redirect} from 'react-router-dom';
 import CanI from '../Permissions';
 
-export const PrivateRoute = ({ component: Component, permission: permission, ...rest }) => {
+export const PrivateRoute = ({component: Component, permission: permission, ...rest}) => {
 	const loggedIn = useSelector(state => state.auth.is_authenticated);
 
 	return (
-			<Route {...rest} render={(props) => (
-				loggedIn === true
-					? <Component {...props} />
-					: <Redirect to={{
-						pathname: '/login',
-						state: {from: props.location}
-					}}/>
-			)}/>
+		<Route {...rest} render={(props) => (
+			loggedIn === true
+				? <Component {...props} />
+				: <Redirect to={{
+					pathname: '/login',
+					state: {from: props.location}
+				}}/>
+		)}/>
 	);
-}
+};
 
 
-export const PrivateRoutePerm = ({permission, ...rest }) => {
+export const PrivateRoutePerm = ({permission, ...rest}) => {
 	return (
 		<CanI perform={permission}>
 			<PrivateRoute {...rest}/>
 		</CanI>
 	);
-}
+};
