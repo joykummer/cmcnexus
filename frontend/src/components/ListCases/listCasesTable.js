d6c5a853c27fb1f6c739345e9b563558a36a2a0b
Joy Yoo <joyannayoo@gmail.com>
2020-04-13 18:27:41 +0200

    add table to cases

--- frontend/src/components/ListCases/listCasesTable.js
+++ frontend/src/components/ListCases/listCasesTable.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import {casesFunction} from "../../store/actions/casesAction";
 import {useHistory} from 'react-router-dom';
+
 import {
   Table,
   TableBody,
