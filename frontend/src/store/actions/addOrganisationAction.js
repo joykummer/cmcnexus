e5a6812bfcab66b3e6550dd9ddb5f06fcbe32653
Joy Yoo <joyannayoo@gmail.com>
2020-04-13 09:27:40 +0200

    include add organisation in store

--- frontend/src/store/actions/addOrganisationAction.js
+++ frontend/src/store/actions/addOrganisationAction.js
@@ -1,9 +1,18 @@
 import Axios from "../../axios/authenticated";
+import {ADD_ORGANISATION} from "./actionTypes";
+
+export const addOrganisationAction = (organisations) => {
+  return {
+    type: ADD_ORGANISATION,
+    payload: organisations,
+  };
+};
+
 
 export const addOrganisationFunction = (data) => async (dispatch) => {
-  console.log("in the add function", data);
   try {
     const response = await Axios.post("organisations/add/", data);
+    dispatch(addOrganisationAction(response.data));
   } catch (e) {
     console.log("e", e);
   }
--- frontend/src/store/reducers/OrganisationsReducer/index.js
+++ frontend/src/store/reducers/OrganisationsReducer/index.js
@@ -1,9 +1,12 @@
-import { SET_ORGANISATIONS } from "../../actions/actionTypes";
+import { SET_ORGANISATIONS, ADD_ORGANISATION } from "../../actions/actionTypes";
 
 export default function organisations(state = [], action) {
   switch (action.type) {
     case SET_ORGANISATIONS: {
       return action.payload;
+    }
+    case ADD_ORGANISATION: {
+      return [...state, action.payload]
     }
     default:
       return state;
