d58f6996bf410958f820a3f0711b9fd6b3a7f63e
Jannic Veith <jannic.veith@v-online.me>
2020-04-16 14:57:55 +0200

    Fix localhost

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 python -c "import time; time.sleep(3)" # Wait for postgres to start up
 python manage.py migrate
+python manage.py create_role_groups
 python manage.py collectstatic --no-input
 rm -rf /frontend/build/* && cp -r /frontend_tmp/* /frontend
 gunicorn -w 4 -b 0.0.0.0:8000 orca.wsgi:application
