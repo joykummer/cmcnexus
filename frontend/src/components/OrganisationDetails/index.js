6400248354aebcf4dc3e01e7223102f78bc15e10
Myri <myrimail84@gmail.comm>
2020-04-16 11:39:19 +0200

    styling organisations details and refactor cases

--- frontend/src/components/OrganisationDetails/index.js
+++ frontend/src/components/OrganisationDetails/index.js
@@ -8,9 +8,61 @@ const Container = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  /* justify-content: center; */
   align-items: center;
 `;
+
+const HeaderTitle = styled.div`
+display: flex; 
+justify-content: center; 
+font-size: 30px; 
+margin-top: 5%; 
+margin-bottom: 3%; 
+border-bottom: 3px solid red; 
+`;
+
+const Stripe = styled.div`
+height: auto;
+width: 20%; 
+background-color: red; 
+display: flex; 
+align-self: end;
+margin-left: 5%;
+font-size: 16px; 
+border-radius: 5px; 
+margin-top: 2%; 
+padding-left: 1%; 
+color: white; 
+text-transform: uppercase; 
+`;
+
+const DetailsContainer = styled.div`
+width: 80%;
+height: auto; 
+display: flex; 
+flex-direction: column; 
+padding: 20px; 
+margin: 2%; 
+background-color: #ebebeb;
+box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+`;
+
+const DetailsContainerCategory = styled(DetailsContainer)`
+background-color: white; 
+box-shadow: none; 
+padding: 0; 
+`
+
+const DetailsContainerSmall = styled.div`
+width: auto;
+display: flex; 
+align-self: flex-start;
+/* margin-left: 10%; */
+padding: 20px; 
+/* margin: 2%;  */
+background-color: #ebebeb;
+box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+`
 
 function OrganisationDetails(props) {
   const dispatch = props.dispatch;
@@ -29,12 +81,23 @@ function OrganisationDetails(props) {
     <Container>
       {organisationDetails ? (
         <>
-          <div>name: {organisationDetails.name}</div>
-          <div>description: {organisationDetails.description}</div>
-          <div>services: {organisationDetails.services}</div>
-          <div>category: {organisationDetails.category.name}</div>
-          <div>tag: {organisationDetails.tag}</div>
-          <div>members: {organisationDetails.members}</div>
+          <HeaderTitle>{organisationDetails.name}</HeaderTitle>
+          <Stripe>description</Stripe>
+          <DetailsContainer>{organisationDetails.description}</DetailsContainer>
+          <Stripe>services</Stripe>
+          <DetailsContainer>{organisationDetails.services}</DetailsContainer>
+          <Stripe>category</Stripe>
+          <DetailsContainerCategory>
+            <DetailsContainerSmall>{organisationDetails.category.name}</DetailsContainerSmall>
+          </DetailsContainerCategory>
+          <Stripe>tag</Stripe>
+          <DetailsContainerCategory>
+            <DetailsContainerSmall>{organisationDetails.tag}</DetailsContainerSmall>
+          </DetailsContainerCategory>
+          <Stripe>members</Stripe>
+          <DetailsContainerCategory>
+            <DetailsContainerSmall>{organisationDetails.members}</DetailsContainerSmall>
+          </DetailsContainerCategory>
         </>
       ) : (
         <div>This organisation does not exist.</div>
