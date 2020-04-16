e8d4784df497213b672e13e84f4ed2bd0d43fac6
Jannic Veith <jannic.veith@v-online.me>
2020-04-16 14:36:35 +0200

    Fix user access permissions

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,7 @@
 let baseUrl = '';
 
 if (process.env.NODE_ENV === 'development') {
-	baseUrl = 'http://localhost:8000/backend/api/'
+	baseUrl = 'http://0.0.0.0:8000/backend/api/'
 } else {
 	baseUrl = 'https://cmc-nexus.propulsion-learn.ch/backend/api/'
 }
