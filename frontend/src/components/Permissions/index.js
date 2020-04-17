38e2a5c85bc122e8252fca2372d9c8a2171cb79c
Jannic Veith <jannic.veith@v-online.me>
2020-04-17 18:59:24 +0200

    Add loader while permissions are loading

--- frontend/src/components/Permissions/index.js
+++ frontend/src/components/Permissions/index.js
@@ -1,15 +1,32 @@
+import React from 'react';
 import {useSelector} from 'react-redux';
+import ClipLoader from "react-spinners/ClipLoader";
+import styled from "styled-components";
 
+
+const Center = styled.div`
+width:100%;
+display: flex;
+justify-content: center;
+align-items: center;
+`;
 
 const CanI = props => {
 	const permissions = useSelector(state => state.auth.user ? state.auth.user.permissions : null);
 	if (!permissions) {
-		return null;
+		return (
+			<Center>
+				<ClipLoader
+					size={35}
+					color={"#ff0000"}
+					loading={true}
+				/>
+			</Center>
+		);
 	}
 	if (permissions && permissions.includes(props.perform)) {
 		return props.children;
 	}
-	console.log("CANNOR", props, permissions)
 	return props.no();
 }
 
