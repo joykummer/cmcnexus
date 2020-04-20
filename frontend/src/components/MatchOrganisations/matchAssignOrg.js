8510a1781cecdb46b2ff271a9710a4786c725fe6
Joy Yoo <joyannayoo@gmail.com>
2020-04-20 22:52:35 +0200

    change match table

--- frontend/src/components/MatchOrganisations/matchAssignOrg.js
+++ frontend/src/components/MatchOrganisations/matchAssignOrg.js
@@ -11,12 +11,19 @@ import {
   TableHeader,
   TableHeaderRow,
   TableHeaderWrapper,
-  TableRow,
 } from "../../styles/Tables";
 import styled from "styled-components";
 import {setNavigationAction} from '../../store/actions/Navigation';
 import {ORGANISATIONS} from '../Navigation/states';
 import {casesFunction} from "../../store/actions/Cases/casesAction";
+
+  const CustomTableRow = styled.tr`
+  width: 100%;
+  text-transform: capitalize;
+  :nth-child(odd) {
+    background: #ebebeb;
+  }
+  `;
 
   const MatchAssignButton = styled(RedButton)`
     width: 150px;
@@ -104,7 +111,7 @@ function MatchAssignOrg(props) {
           {organisationsMatchingByCategory
             ? organisationsMatchingByCategory.map((organisation) => {
                 return (
-                  <TableRow key={organisation.id}>
+                  <CustomTableRow key={organisation.id}>
                     <TableData>{organisation.name}</TableData>
                     <TableData>{organisation.description}</TableData>
                     <TableData>{organisation.categories.map((cat) => cat.name).join(', ')}</TableData>
@@ -115,7 +122,7 @@ function MatchAssignOrg(props) {
                     <TableData>
                       <AssignActionable dispatch={props.dispatch} organisation={organisation} singleCase={singleCase}/>
                     </TableData>
-                  </TableRow>
+                  </CustomTableRow>
                 );
               })
             : null}
