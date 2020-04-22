449a6b26402c4f083cd36045720ae266abe022e0
Greta Zurkowska <greta.zurkowska@gmail.com>
2020-04-22 16:54:02 +0200

    Fix overflow scroll.

--- frontend/src/styles/BaseContainer/index.js
+++ frontend/src/styles/BaseContainer/index.js
@@ -5,7 +5,6 @@ export const Container = styled.div`
   height: 100%;
   background-color: #EBEBEB;
   padding: 40px 60px 40px 60px;
-  overflow: scroll;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
