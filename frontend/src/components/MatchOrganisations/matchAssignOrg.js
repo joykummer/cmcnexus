e823007c2d59cfd4eb8599f36669f9bb785551d2
Joy Yoo <joyannayoo@gmail.com>
2020-04-22 16:38:50 +0200

    change small details

--- frontend/src/components/MatchOrganisations/matchAssignOrg.js
+++ frontend/src/components/MatchOrganisations/matchAssignOrg.js
@@ -86,7 +86,7 @@ function MatchAssignOrg(props) {
   };
 
   const organisationsMatchingByCategory = filteredOrganisations()
-  const headers = ["Name", "Description", "Category", "Tag(s)"];
+  const headers = ["Name", "Description", "Category", "Tag"];
 
   return (
       <Table>
